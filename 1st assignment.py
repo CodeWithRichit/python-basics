1b729b00cabc9115348a54ccfdea5d5ad671be39
CodeWithRichit <richittyagi@gmail.com>
2025-06-16 06:06:25 +0530

    Add files via upload

---
+++ 1st assignment.py
@@ -0,0 +1,11 @@
+#Task 1;Basic Mathematical Operations
+Num1=int(input("Number1: "))
+Num2=int(input("Number2: "))
+Add=print("Addition: ",Num1+Num2)
+Sub=print("Subtraction: ",Num1-Num2)
+Into=print("Multiplication: ",Num1*Num2)
+Div=print("Division: ",Num1/Num2)
+#Task 2; Create a Personalised Greeting
+Fname=input("Enter your First name: ")
+Lname=input("Enter your Last name: ")
+print("Hello, "+Fname+Lname+"! Welcome to the python program.")
